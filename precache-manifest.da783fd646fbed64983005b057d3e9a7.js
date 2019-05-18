a4b75e82099580c540b4489f4df40b06f5d55954
Kiril Serchenko <kirils@amdocs.com>
2019-05-18 15:32:44 +0300

    Updates

---
+++ precache-manifest.da783fd646fbed64983005b057d3e9a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a363c2f014f752eb88ccf0cd389d0c40",
+    "url": "/KirillSerchenko/amd.git/index.html"
+  },
+  {
+    "revision": "af0def7246f7af181791",
+    "url": "/KirillSerchenko/amd.git/static/css/2.6749d01e.chunk.css"
+  },
+  {
+    "revision": "37282c3109ee48c16e97",
+    "url": "/KirillSerchenko/amd.git/static/css/main.f702abf9.chunk.css"
+  },
+  {
+    "revision": "af0def7246f7af181791",
+    "url": "/KirillSerchenko/amd.git/static/js/2.67e10b9f.chunk.js"
+  },
+  {
+    "revision": "37282c3109ee48c16e97",
+    "url": "/KirillSerchenko/amd.git/static/js/main.67a9017e.chunk.js"
+  },
+  {
+    "revision": "c10e2693e33fc7008ba7",
+    "url": "/KirillSerchenko/amd.git/static/js/runtime~main.c4a2818c.js"
+  },
+  {
+    "revision": "85a22f4107a5b26244cdea9b7a5402d0",
+    "url": "/KirillSerchenko/amd.git/static/media/amdocs.85a22f41.jpg"
+  },
+  {
+    "revision": "0067fc97f477ae7ce2e932d1bac762a6",
+    "url": "/KirillSerchenko/amd.git/static/media/amdocsDash.0067fc97.jpg"
+  }
+]);
